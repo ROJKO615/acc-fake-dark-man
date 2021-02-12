f20caea657243739f699dfb8f31941de6f0135c0
ROJKO615 <77924335+ROJKO615@users.noreply.github.com>
2021-02-12 20:52:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7387,7 +7387,7 @@ message.member.voiceChannel
 
 //تۆكینه‌كان لێره‌ جا بنێ
 
-client.login("");
+client.login("ODA5ODQyMzI5MjE3MDA3NjU2.YCa_WQ.Q6sdDmlzACnNV_zjBwdZnhy5Ne8");
 client2.login("");
 client3.login("");
 client4.login("");
